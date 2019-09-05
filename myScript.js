802fc572a7e3d75543b534ac88b729c5b352e1ff
leon-zaporski <32162982+leon-zaporski@users.noreply.github.com>
2019-09-05 14:54:01 +0200

    Update myScript.js

--- myScript.js
+++ myScript.js
@@ -5,7 +5,7 @@ var indices = Array.from(Array(lenarr).keys())
 
 var white_n = [];
 for (var i = 0; i < indices.length; i++) {
-     white_n.push(Math.random());
+     white_n.push(Math.round(Math.random()));
 }
 
 TESTER = document.getElementById('tester');
