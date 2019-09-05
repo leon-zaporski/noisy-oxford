dc698a685af8501d1ae6762fb295245db0193ab6
leon-zaporski <32162982+leon-zaporski@users.noreply.github.com>
2019-09-05 13:29:15 +0200

    Update myScript.js

--- myScript.js
+++ myScript.js
@@ -1,7 +1,3 @@
-var slider = document.getElementById("myRange");
-var output = document.getElementById("demo");
-output.innerHTML = slider.value;
-
-slider.oninput = function() {
-  output.innerHTML = this.value;
+function myFunction() {
+  document.getElementById("demo").innerHTML = "Paragraph changed.";
 }
