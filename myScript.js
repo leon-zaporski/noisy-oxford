bc6b0ebacca4c262ec6ec692a630c4c586a57c7f
leon-zaporski <32162982+leon-zaporski@users.noreply.github.com>
2019-09-05 15:07:19 +0200

    Update myScript.js

--- myScript.js
+++ myScript.js
@@ -1,11 +1,15 @@
+function randn_bm() {
+    var u = 0, v = 0;
+    while(u === 0) u = Math.random(); //Converting [0,1) to (0,1)
+    while(v === 0) v = Math.random();
+    return Math.sqrt( -2.0 * Math.log( u ) ) * Math.cos( 2.0 * Math.PI * v );
+}
 
-
-var lenarr = 100
+var lenarr = 100000
 var indices = Array.from(Array(lenarr).keys())
-
 var white_n = [];
 for (var i = 0; i < indices.length; i++) {
-     white_n.push(Math.round(Math.random()));
+     white_n.push(randn_bm());
 }
 
 TESTER = document.getElementById('tester');
